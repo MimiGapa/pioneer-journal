e72734fa7c64580e144dabe0e217b474446a3d89
MimiGapa <mimigapasangra@gmail.com>
2025-04-14 04:02:02 +0800

    Fix resource loading issues

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true
-  },
-  base: '/pioneer-journal/' // Required for GitHub Pages
+  }
+  // No need for base path like with GitHub Pages!
 });
